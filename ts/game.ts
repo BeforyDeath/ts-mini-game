79a48dedaae51b8932f5bf3b399a0196d7b4124b
BeforyDeath <beforydeath@gmail.com>
2016-05-09 13:02:03 +0300

    вариант анимации с jquery

--- ts/game.ts
+++ ts/game.ts
@@ -2,7 +2,8 @@
 
 class Game {
     level:number;
-    list:number[] = [];
+    list:string[] = [];
+    direction:string[] = [];
 
     constructor(level:number = 4) {
         this.level = level;
@@ -35,6 +36,7 @@ class Game {
         var col = parseInt(block.id[1]);
 
         this.list = [];
+        this.direction = [];
         for (var cell = 1; cell < this.level + 1; cell++) {
             this.up(row, col, cell);
             this.right(row, col, cell);
@@ -46,40 +48,54 @@ class Game {
         for (let i in this.list) {
             setTimeout(function () {
                 var id = _self.list[i];
+                var direction = _self.direction[i];
                 block = document.getElementById(id);
-                _self.toggle(block);
-            }, 30 * i)
+                _self.toggle(block, direction);
+            }, 20 * i)
         }
-
     };
 
-    toggle = (block) => {
+    toggle = (block, direction) => {
         var state = block.getAttribute('state');
         if (state == 0) block.setAttribute("state", "1");
         else block.setAttribute("state", "0");
-        block.classList.toggle("state_0");
-        block.classList.toggle("state_1");
+
+        $(block).animate({opacity: "0"}, 60, function () {
+            block.classList.toggle("state_0");
+            block.classList.toggle("state_1");
+        }).animate({opacity: "1"}, 60);
+
     };
 
     down = (row, col, cell) => {
         var n = (row) + cell;
-        if (n < (this.level + 1)) this.list.push('' + n + col);
+        if (n < (this.level + 1)) {
+            this.list.push('' + n + col);
+            this.direction.push('down');
+        }
     };
 
     up = (row, col, cell) => {
         var n = (row) - cell;
-        if (n > 0) this.list.push('' + n + col);
+        if (n > 0) {
+            this.list.push('' + n + col);
+            this.direction.push('up');
+        }
     };
 
     left = (row, col, cell) => {
         var n = (col) - cell;
-        if (n > 0) this.list.push('' + row + n);
+        if (n > 0) {
+            this.list.push('' + row + n);
+            this.direction.push('left');
+        }
     };
 
     right = (row, col, cell) => {
         var n = (col) + cell;
-        if (n < (this.level + 1)) this.list.push('' + row + n);
+        if (n < (this.level + 1)) {
+            this.list.push('' + row + n);
+            this.direction.push('right');
+        }
     };
-
-
 }
